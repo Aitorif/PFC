fb9f2d3839d838ab457dcfb67b1ef2df6ce92cc7
Aitorif <78684596+Aitorif@users.noreply.github.com>
2023-12-10 11:20:48 +0100

    Update bdclinica.sql

--- bdclinica.sql
+++ bdclinica.sql
@@ -19,6 +19,39 @@
 CREATE DATABASE IF NOT EXISTS `clinica` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
 USE `clinica`;
 
+
+-- Volcando estructura para tabla clinica.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `apellidos` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `pass` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `photo` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'userphoto.png',
+  `direccion` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `dni` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `rol` varchar(5) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'user',
+  `trabajador` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0',
+  `telefono` varchar(12) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- Volcando datos para la tabla clinica.user: ~5 rows (aproximadamente)
+DELETE FROM `user`;
+INSERT INTO `user` (`id`, `nombre`, `apellidos`, `email`, `pass`, `photo`, `direccion`, `dni`, `rol`, `trabajador`, `telefono`) VALUES
+	(3, 'Aitor', 'Iglesias Franjo', 'aitor@clinicacastineira.com', 'aitor', '3foto.png', 'c/Ángel del Castillo, 29', '638184210', 'admin', 'true', NULL),
+	(8, 'Juan', 'Gomez Perez', 'juan@gmail.com', 'contraseña1', 'foto1.png', 'Calle 123, Ciudad A', '123456789A', 'user', 'false', '123-456-789'),
+	(9, 'Maria', 'Lopez Garcia', 'maria@yahoo.com', 'contraseña2', 'foto2.png', 'Avenida XYZ, Ciudad B', '987654321B', 'user', 'false', '987-654-321'),
+	(10, 'Pedro', 'Martinez Rodriguez', 'pedro@hotmail.com', 'contraseña3', 'foto3.png', 'Calle Principal, Ciudad C', '111111111C', 'admin', 'true', '111-222-333'),
+	(11, 'Laura', 'Sánchez Fernández', 'laura@gmail.com', 'contraseña4', 'foto4.png', 'Carrera 456, Ciudad D', '222222222D', 'admin', 'true', '444-555-666');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+
+
 -- Volcando estructura para tabla clinica.citas
 CREATE TABLE IF NOT EXISTS `citas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -31,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `citas` (
   KEY `id_paciente` (`id_paciente`),
   CONSTRAINT `FK_citas_user` FOREIGN KEY (`id_trabajador`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_citas_user_2` FOREIGN KEY (`id_paciente`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- Volcando datos para la tabla clinica.citas: ~4 rows (aproximadamente)
 DELETE FROM `citas`;
@@ -44,7 +77,7 @@ INSERT INTO `citas` (`id`, `id_trabajador`, `id_paciente`, `dia`, `hora`) VALUES
 -- Volcando estructura para tabla clinica.citas_posibles
 CREATE TABLE IF NOT EXISTS `citas_posibles` (
   `hora` varchar(50) NOT NULL DEFAULT ''
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- Volcando datos para la tabla clinica.citas_posibles: ~18 rows (aproximadamente)
 DELETE FROM `citas_posibles`;
@@ -119,7 +152,7 @@ CREATE TABLE IF NOT EXISTS `documentos` (
   PRIMARY KEY (`id`),
   KEY `FK_propietario` (`propietario`),
   CONSTRAINT `FK_documentos_user` FOREIGN KEY (`propietario`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- Volcando datos para la tabla clinica.documentos: ~5 rows (aproximadamente)
 DELETE FROM `documentos`;
@@ -141,7 +174,7 @@ CREATE TABLE IF NOT EXISTS `documento_compartido` (
   CONSTRAINT `FK_documento_compartido_user` FOREIGN KEY (`id_trabajador`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   CONSTRAINT `FK_id_doc` FOREIGN KEY (`id_documento`) REFERENCES `documentos` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   CONSTRAINT `FK_id_usr` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- Volcando datos para la tabla clinica.documento_compartido: ~5 rows (aproximadamente)
 DELETE FROM `documento_compartido`;
@@ -164,7 +197,7 @@ CREATE TABLE IF NOT EXISTS `facturas` (
   PRIMARY KEY (`id`),
   KEY `FK1id_user` (`id_user`),
   CONSTRAINT `FK1id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- Volcando datos para la tabla clinica.facturas: ~15 rows (aproximadamente)
 DELETE FROM `facturas`;
@@ -184,34 +217,3 @@ INSERT INTO `facturas` (`id`, `fecha`, `descripcion`, `id_user`, `cantidad`, `pr
 	(000019, '2023-12-19', 'Consulta médica', 11, 1, 50, 50),
 	(000020, '2023-12-20', 'Rehabilitación neurológica', 11, 3, 30, 90),
 	(000021, '2023-12-21', 'Control de medicamentos', 11, 1, 20, 20);
-
--- Volcando estructura para tabla clinica.user
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `apellidos` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `email` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `pass` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `photo` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'userphoto.png',
-  `direccion` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `dni` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `rol` varchar(5) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'user',
-  `trabajador` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0',
-  `telefono` varchar(12) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- Volcando datos para la tabla clinica.user: ~5 rows (aproximadamente)
-DELETE FROM `user`;
-INSERT INTO `user` (`id`, `nombre`, `apellidos`, `email`, `pass`, `photo`, `direccion`, `dni`, `rol`, `trabajador`, `telefono`) VALUES
-	(3, 'Aitor', 'Iglesias Franjo', 'aitor@clinicacastineira.com', 'aitor', '3foto.png', 'c/Ángel del Castillo, 29', '638184210', 'admin', 'true', NULL),
-	(8, 'Juan', 'Gomez Perez', 'juan@gmail.com', 'contraseña1', 'foto1.png', 'Calle 123, Ciudad A', '123456789A', 'user', 'false', '123-456-789'),
-	(9, 'Maria', 'Lopez Garcia', 'maria@yahoo.com', 'contraseña2', 'foto2.png', 'Avenida XYZ, Ciudad B', '987654321B', 'user', 'false', '987-654-321'),
-	(10, 'Pedro', 'Martinez Rodriguez', 'pedro@hotmail.com', 'contraseña3', 'foto3.png', 'Calle Principal, Ciudad C', '111111111C', 'admin', 'true', '111-222-333'),
-	(11, 'Laura', 'Sánchez Fernández', 'laura@gmail.com', 'contraseña4', 'foto4.png', 'Carrera 456, Ciudad D', '222222222D', 'admin', 'true', '444-555-666');
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
